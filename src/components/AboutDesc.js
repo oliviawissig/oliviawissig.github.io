f3008771681a9ff961344e9a2066833cd6a0a6d3
Olivia Wissig <oliviawissig@gmail.com>
2023-07-04 19:25:58 -0700

    fixed more mobile stuff, made prettier

--- src/components/AboutDesc.js
+++ src/components/AboutDesc.js
@@ -8,18 +8,21 @@ function AboutDesc() {
     return (<div className="about">
         <div id="text">
             <p>
-                i am a striving developer, with a bachelor's degree in computer science. currently, i am working in
-                technical solutions at <a href="https://www.openweb.com">OpenWeb</a>. i am passionate about
-                technology, sneakers, and plants.
+                hi, welcome! my name is olivia, and i am currently working as a support engineer at <a
+                href="https://www.openweb.com">OpenWeb</a>.
+                i was born and raised in the bay area and i am now living in the sunny los angeles with my partner,
+                matthew, and our two pups.
+            </p>
+            <p style={{textTransform: "lowercase"}}>
+                i first got into HTML and web development when I started using Tumblr and wanted to customize my blog. I
+                quickly found that
+                I enjoyed the challenge and decided to take an intro to Java class in high school. From there, I was
+                completely
+                drawn to programming and have been passionate about it ever since
             </p>
             <p>
-                i was first introduced to html when i joined tumblr and began customizing my blog. after learning
-                how much i enjoyed coding, i found myself enrolled in an "intro to java" class in high school and
-                that's when I was drawn to programming.
-            </p>
-            <p>
-                i am passionate about front-end development, and i like to focus on an easy and convenient user
-                experience. i have experience or have completed projects in the following languages:
+                after trying different projects and languages, i found am more interested and more passionate about front-end development. i like to make sure the user experience is easy and
+                convenient. i focus on working in react and javascript, but i have experience or have completed projects in the following languages:
             </p>
             <span className="skills">
                       <FontAwesomeIcon className="skill" icon="fa-brands fa-html5"/>
@@ -30,8 +33,10 @@ function AboutDesc() {
                       <FontAwesomeIcon className="skill" icon="fa-brands fa-python"/>
                 </span>
         </div>
-        <div className="blur-load blur-load-me" style={{backgroundImage: "url(" + meS + ")", height:270, width:270, borderRadius:"8px"}}>
-            <img src={me} alt="me holding a flower bouquet" height={270} width={270} className="me" loading={"lazy"}></img>
+        <div className="blur-load blur-load-me"
+             style={{backgroundImage: "url(" + meS + ")", height: 270, width: 270, borderRadius: "8px"}}>
+            <img src={me} alt="me holding a flower bouquet" height={270} width={270} className="me"
+                 loading={"lazy"}></img>
         </div>
     </div>);
 }
