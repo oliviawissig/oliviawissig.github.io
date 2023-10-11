73c3839f3cfe580e8823281730966edd4ac1b78a
Olivia Wissig <oliviawissig@gmail.com>
2023-10-11 16:58:51 -0700

    general update

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import {library} from '@fortawesome/fontawesome-svg-core'
+import Form from './Form';
+import PDF from '../assets/resume.pdf';
+import React from 'react';
+
+import "../styles/Footer.css"
+
+library.add()
+
+function Footer() {
+
+    return ( 
+        <footer className="footer">
+            <div className="footerContent">
+                <div className="footerContainer">
+                    <div className="contactMe">
+                        <h3>Contact Me</h3>
+                        <Form/>
+                    </div>
+                    <div className="footerLinks">
+                        <h3>Quick Links</h3>
+                        <a href="https://www.linkedin.com/in/oliviawissig/">LinkedIn</a>
+                        <a href="https://github.com/oliviawissig">Github</a>
+                        <a href="https://www.facebook.com/oliviawissig">Facebook</a>
+                        <a href={PDF}>Resume</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+     );
+}
+
+export default Footer;
